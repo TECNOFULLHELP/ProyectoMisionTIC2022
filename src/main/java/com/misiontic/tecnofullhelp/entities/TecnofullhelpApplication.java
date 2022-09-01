9125b05fd1c7c9e31b45003c7171ec91c09947bc
William Mantilla <william.mantilla14@gmail.com>
2022-09-01 00:04:36 -0500

    Se incluyen en main test clases Enterprise, Employee, Transaction

--- src/main/java/com/misiontic/tecnofullhelp/entities/TecnofullhelpApplication.java
+++ src/main/java/com/misiontic/tecnofullhelp/entities/TecnofullhelpApplication.java
@@ -8,8 +8,9 @@ public class TecnofullhelpApplication {
 
 	public static void main(String[] args) {
 		//SpringApplication.run(TecnofullhelpApplication.class, args);
-		//Instanciar objeto de la clase Empresa
+
 		System.out.println("------------------------------------------------Test Empresa------------------------------------------------");
+		//Instanciar objeto de la clase Empresa
 		Enterprise Empresa1 = new Enterprise(98090909,"Bancolombia", "DIAG 21B # 1 - 33", "3132525622");
 		Enterprise Empresa2 = new Enterprise(88888888,"Finanzas S.A.", "CRA 21 # 1 - 35", "3127894545");
 		//Valores iniciales
@@ -42,12 +43,11 @@ public class TecnofullhelpApplication {
 		//Valor final
 		System.out.println(Empresa1.toString());
 
-		//Instanciar objeto de la clase Empresa
 		System.out.println("------------------------------------------------Test Empleado------------------------------------------------");
-
 		//Instanciando objeto de tipo Empleado
 		Employee Empleado1 = new Employee("Emmanuel Mantilla","emmanuel15@bancolombia.com", Empresa1, Enum_role.Operario);
-
+		Employee Empleado2 = new Employee("Adriana Perez","adriana@bancolombia.com", Empresa1, Enum_role.Operario);
+		Employee Empleado3 = new Employee("Kevin Moreno","kevin@finanzas.com", Empresa2, Enum_role.Operario);
 		//Valores iniciales
 		System.out.println(Empleado1.toString());
 
@@ -77,6 +77,41 @@ public class TecnofullhelpApplication {
 
 		//Valor final
 		System.out.println(Empleado1.toString());
+
+		System.out.println("------------------------------------------------Test Transaction------------------------------------------------");
+		//Instanciando objeto de tipo Transaccion
+		Transaction transaccion1 = new Transaction(-190000,"Pagos Servicios", Empleado1);
+
+		//Valores iniciales
+		System.out.println(transaccion1.toString());
+
+		//Leer monto movimiento
+		System.out.println("monto transaccion1: " + transaccion1.getAmount());
+		//Modificar monto movimiento
+		transaccion1.setAmount(-2000000);
+		System.out.println("monto movimiento actualizado: " + transaccion1.getAmount());
+
+		//crear monto negativo
+		Transaction transaccion2 = new Transaction(-75000,"Pagos papeleria", Empleado2);
+		// crear monto positivo
+		Transaction transaccion3 = new Transaction(1500000,"Arrendamiento licencias software", Empleado3);
+
+		//Leer concepto
+		System.out.println("Concepto transaccion1: " + transaccion1.getConcept());
+		//Modificar concepto
+		transaccion1.setConcept("Pago servicios Agua y Luz");
+		System.out.println("concepto transaccion1 actualizada: " + transaccion1.getConcept());
+
+		//Consultar usuario movimientos
+		System.out.println("usuario realizo transaccion1: " + transaccion1.getUser().getName());
+		System.out.println("usuario realizo transaccion2: " + transaccion1.getUser().getName());
+		System.out.println("usuario realizo transaccion3: " + transaccion1.getUser().getName());
+
+		//movimientos final
+		System.out.println("***Resumen Transacciones***");
+		System.out.println(transaccion1.toString());
+		System.out.println(transaccion2.toString());
+		System.out.println(transaccion3.toString());
 
 	}
 
